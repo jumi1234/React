95beb156ac4fe3ec902557ba3b46a762f06579db
jumi1234 <farewellsad89@gmail.com>
2020-07-22 14:14:17 +0900

    redux complete

--- src/App.js
+++ src/App.js
@@ -2,14 +2,54 @@ import React from "react";
 import "./App.css";
 import InputBox from "./components/InputBox";
 import PhoneList from "./components/PhoneList";
+import { connect } from "react-redux";
+import { bindActionCreators } from "redux";
+import * as inputFunc from "./store/modules/input";
+import * as dataFunc from "./store/modules/data";
 
-const App = () => {
+const App = ({ name, phone, data, inputFunc, dataFunc }) => {
+
+  const handleChange = e => {
+    const { name, value } = e.target;
+    inputFunc.setInputValue({ name, value });
+  }
+
+  const handleSubmit = () => {
+    if (name === "" || phone === "") return;
+
+    dataFunc.appendData({
+      name, phone
+    });
+    inputFunc.setInputValue({
+      name: 'name',
+      value: ''
+    });
+    inputFunc.setInputValue({
+      name: 'phone',
+      value: ''
+    });
+  }
+
+  const handleRemove = id => {
+    dataFunc.removeData(id);
+  }
+
   return(
     <div className="container">
-      <InputBox />
-      <PhoneList />
+      <InputBox name={name} phone={phone} onChange={handleChange} onSubmit={handleSubmit}/>
+      <PhoneList list={data} deleteItem={handleRemove}/>
     </div>
   )
 }
 
-export default App;
+export default connect(
+    state => ({
+      name: state.input.name,
+      phone: state.input.phone,
+      data: state.data
+    }),
+    dispatch => ({
+      inputFunc: bindActionCreators(inputFunc, dispatch),
+      dataFunc: bindActionCreators(dataFunc, dispatch)
+    })
+)(App);
--- src/components/InputBox/InputBox.js
+++ src/components/InputBox/InputBox.js
@@ -1,20 +1,20 @@
 import React from "react";
 import "./InputBox.css";
 
-const InputBox = ({ name, phone, onChangeName, onChangePhone, onSubmit }) => {
+const InputBox = ({ name, phone, onChange, onSubmit }) => {
   return (
     <div className="input_boxes">
       <div className="input_box">
         <div cassName="input_box_name">
           이름
         </div>
-        <input type="text" placeholder="이름" name="name" className="input_box_input" onChange={onChangeName} value={name} />
+        <input type="text" placeholder="이름" name="name" className="input_box_input" onChange={onChange} value={name} />
       </div>
       <div className="input_box">
         <div cassName="input_box_name">
           전화번호
         </div>
-        <input type="text" placeholder="전화번호" name="phone" className="input_box_input" onChange={onChangePhone} value={phone} />
+        <input type="text" placeholder="전화번호" name="phone" className="input_box_input" onChange={onChange} value={phone} />
       </div>
       <button className="input_box_button" onClick={onSubmit}>저장</button>
     </div>
